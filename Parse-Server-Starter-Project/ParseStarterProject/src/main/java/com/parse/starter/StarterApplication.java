e2d5ee705d6c115682853f6a7902a891c7a6fe26
Liuying Wang <wly@Liuyings-MacBook-Pro.local>
2021-04-23 22:55:35 -0700

    log in page

--- Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -39,23 +39,23 @@ public class StarterApplication extends Application {
             .build()
     );
 
-    ParseObject object = new ParseObject("ExampleObject");
-    object.put("myNumber", "123");
-    object.put("myString", "rob2");
+//    ParseObject object = new ParseObject("ExampleObject");
+//    object.put("myNumber", "123");
+//    object.put("myString", "rob2");
+//
+//    object.saveInBackground(new SaveCallback () {
+//      @Override
+//      public void done(ParseException ex) {
+//        if (ex == null) {
+//          Log.i("Parse Result", "Successful!");
+//        } else {
+//          Log.i("Parse Result", "Failed" + ex.toString());
+//        }
+//      }
+//    });
 
-    object.saveInBackground(new SaveCallback () {
-      @Override
-      public void done(ParseException ex) {
-        if (ex == null) {
-          Log.i("Parse Result", "Successful!");
-        } else {
-          Log.i("Parse Result", "Failed" + ex.toString());
-        }
-      }
-    });
 
-
-    ParseUser.enableAutomaticUser();
+    //ParseUser.enableAutomaticUser();
 
     ParseACL defaultACL = new ParseACL();
     defaultACL.setPublicReadAccess(true);
